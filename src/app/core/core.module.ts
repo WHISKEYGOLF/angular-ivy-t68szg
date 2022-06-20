e6e9d5a6295f3211985db39ffc6a7a77b44f5bd6
WHISKEYGOLF <softwareengineer.pr011@gmail.com>
2022-06-20 16:01:24 -0700

    Hero List & Hero Details: 4 files modified, 4 added

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [HeroesComponent],
-  exports: [HeroesComponent],
+  declarations: [HeroesComponent, HeroComponent],
+  exports: [HeroesComponent, HeroComponent],
 })
 export class CoreModule {}
--- src/app/core/heroes/heroes.component.ts
+++ src/app/core/heroes/heroes.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent {
+  heroes = HEROES;
+  // heroes: Hero[] = [
+  //   { id: 12, name: 'Dr. Nice' },
+  //   { id: 13, name: 'Bombasto' },
+  //   { id: 14, name: 'Celeritas' },
+  //   { id: 15, name: 'Magneta' },
+  //   { id: 16, name: 'RubberMan' },
+  //   { id: 17, name: 'Dynama' },
+  //   { id: 18, name: 'Dr. IQ' },
+  //   { id: 19, name: 'Magma' },
+  //   { id: 20, name: 'Tornado' },
+  // ];
+  selectedHero?: Hero;
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
-  constructor() { }
-
-  ngOnInit(): void {
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
-
 }
