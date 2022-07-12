c806c184ae80de3bdb2308cda4d725d4b773c496
WHISKEYGOLF <softwareengineer.pr011@gmail.com>
2022-07-12 12:23:39 -0700

    hero.service: created getMockHeroResponse Fn- 1 file modified

--- src/app/core/heroes/hero.service.ts
+++ src/app/core/heroes/hero.service.ts
@@ -1,21 +1,59 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, of } from 'rxjs';
+import { BehaviorSubject, catchError, Observable, of, Subject, tap, throwError } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from '../../shared/message.service';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
+  heroList: Hero[] = [];
+  itemsChanged = new Subject<Hero[]>();
   private heroes = new BehaviorSubject<Hero[] | null>(HEROES);
   $heroes = this.heroes.asObservable();
 
-  constructor(private messageService: MessageService) {}
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+    ) {}
+
+  addToHeroList(hero: Hero) {
+    this.heroList = [...this.heroList, hero];
+    this.itemsChanged.next(this.heroList);
+  }
+
+  clearCart() {
+    alert('clearing cart ... ');
+    this.heroList = [];
+    this.itemsChanged.next([]);
+  }
 
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
     return this.$heroes; // heroes
+  }
+
+  getMockHeroResponse() {
+    return this.http
+      .get<{ type: string; price: number }[]>('/heroes/mock-heroes.json')
+      .pipe(
+        catchError(this.handleError),
+        tap((priceData) => {
+          alert(JSON.stringify(priceData));
+        })
+      );
+  }
+
+  private handleError(errorRes: HttpErrorResponse): Observable<never> {
+    let errorMessage = errorRes.error.error.message;
+
+    if (!errorRes.error || !errorRes.error.error) {
+      errorMessage = 'An unknow error occured.';
+      return throwError(errorMessage);
+    }
+    return throwError(errorMessage);
   }
 }
 
